914997a2cb7a0926007956f3d3496db27289dfd0
mark reeves <toj12031@bcaoo.com>
2021-06-22 23:54:02 +0530

    to github id

---
+++ lib/common_widget/AppBarWidget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/components/AppSignIn.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+Widget appBarWidget(context) {
+  return AppBar(
+    elevation: 0.0,
+    centerTitle: true,
+    // title: Image.asset(
+    //   "assets/images/ic_app_icon.png",
+    //   width: 80,
+    //   height: 40,
+    // ),
+    title: Text(
+      "RegeX-World",
+      style: TextStyle(
+          color: Colors.blueGrey, fontWeight: FontWeight.bold, fontSize: 25),
+    ),
+    actions: <Widget>[
+      IconButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => AppSignIn()),
+          );
+        },
+        icon: Icon(FontAwesomeIcons.user),
+        color: Color(0xFF323232),
+      ),
+    ],
+  );
+}
